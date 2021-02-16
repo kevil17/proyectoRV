8b507790bb86610f03a0c3801ff76862d452de9c
kevil17 <kevin.inck@gmail.com>
2021-02-16 15:20:11 -0400

    esta funcionando

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { NavController, Platform  } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  matches: String[];
+  isRecording = false;
+  constructor( public navCtrl: NavController,private speechRecognition: SpeechRecognition,
+    private plt: Platform, private cd: ChangeDetectorRef ) {}
+  isIos() {
+    return this.plt.is('ios');
+  }
+ 
+  stopListening() {
+    this.speechRecognition.stopListening().then(() => {
+      this.isRecording = false;
+    });
+  }
+ 
+  getPermission() {
+    this.speechRecognition.hasPermission()
+      .then((hasPermission: boolean) => {
+        if (!hasPermission) {
+          this.speechRecognition.requestPermission();
+        }
+      });
+  }
+ 
+  startListening() {
+    let options = {
+      language: 'en-US'
+    }
+    this.speechRecognition.startListening().subscribe(matches => {
+      this.matches = matches;
+      this.cd.detectChanges();
+    });
+    this.isRecording = true;
+  }
 }
